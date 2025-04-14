1ad50610284c316abbfe83d27149059706fc6f51
Onur Dabanca <onurdbnc@gmail.com>
2025-04-14 22:06:18 +0300

    Dummy data inserted.

--- WebAPI/DataAccess/DataContext.cs
+++ WebAPI/DataAccess/DataContext.cs
@@ -6,4 +6,24 @@ namespace WebAPI.DataAccess;
 public class DataContext(DbContextOptions options) : DbContext(options) //class seviyesinde constructor.
 {
     public DbSet<Product> Products => Set<Product>(); //Null promise.
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>().HasData(
+            new List<Product>{
+                new Product { Id=1, Name="IPhone 16", Description="Telefon açıklaması", ImageUrl="1.jpg",
+                Price=70000, IsActive=true, Stock=100},
+                new Product { Id=2, Name="IPhone 16 Pro", Description="Telefon açıklaması", ImageUrl="2.jpg",
+                Price=80000, IsActive=true, Stock=200},
+                new Product { Id=3, Name="IPhone 16 Pro Max", Description="Telefon açıklaması", ImageUrl="3.jpg",
+                Price=90000, IsActive=true, Stock=300},
+                new Product { Id=4, Name="IPhone 16 Pro Max Oxi", Description="Telefon açıklaması", ImageUrl="4.jpg",
+                Price=100000, IsActive=true, Stock=400},
+                new Product { Id=5, Name="IPhone 16 Pro Max Oxi Action", Description="Telefon açıklaması", ImageUrl="5.jpg",
+                Price=110000, IsActive=true, Stock=500}
+            }
+        );
+    }
 }
---
+++ WebAPI/Migrations/20250414190430_TestData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "IsActive", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 1, "Telefon açıklaması", "1.jpg", true, "IPhone 16", 70000m, 100 },
+                    { 2, "Telefon açıklaması", "2.jpg", true, "IPhone 16 Pro", 80000m, 200 },
+                    { 3, "Telefon açıklaması", "3.jpg", true, "IPhone 16 Pro Max", 90000m, 300 },
+                    { 4, "Telefon açıklaması", "4.jpg", true, "IPhone 16 Pro Max Oxi", 100000m, 400 },
+                    { 5, "Telefon açıklaması", "5.jpg", true, "IPhone 16 Pro Max Oxi Action", 110000m, 500 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
