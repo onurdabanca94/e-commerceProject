913961095d8a1311fda6c5e93f42cae017c58a1b
Onur Dabanca <onurdbnc@gmail.com>
2025-04-16 01:19:11 +0300

    React project Client created. & dummy insterted.

---
+++ Client/src/App.tsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <>
+      <Header />
+      <ProductList />
+    </>
+  );
+}
+
+function Header() {
+  return <h1>Header</h1>;
+}
+
+function ProductList() {
+  return (
+    <div>
+      <h2>ProductList</h2>
+      <Product />
+    </div>
+  );
+}
+
+function Product() {
+  return <h3>Product</h3>;
+}
+
+export default App;
