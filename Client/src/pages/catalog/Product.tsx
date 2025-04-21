5d7416e5c74b29e0e7b00b17f126ec34ba9a8541
Onur Dabanca <onurdbnc@gmail.com>
2025-04-21 17:37:47 +0300

    ProductDetails & CatalogPage page renewed. Circular progress included.

--- Client/src/pages/catalog/Product.tsx
+++ Client/src/pages/catalog/Product.tsx
@@ -2,6 +2,7 @@ import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@
 import { IProduct } from "../model/IProduct";
 import { AddShoppingCart } from "@mui/icons-material";
 import SearchIcon from '@mui/icons-material/Search';
+import { Link } from "react-router";
 interface Props {
   product: IProduct;
 }
@@ -23,7 +24,7 @@ export default function Product({ product }: Props) {
       </CardContent>
       <CardActions>
         <Button variant="outlined" size="small" startIcon={<AddShoppingCart/>} color="success" >Sepete Ekle</Button>
-        <Button variant="outlined" size="small" startIcon={<SearchIcon/>} color="primary" >Görüntüle</Button>
+        <Button component={Link} to={`/catalog/${product.id}`} variant="outlined" size="small" startIcon={<SearchIcon/>} color="primary" >Görüntüle</Button>
       </CardActions>
     </Card>
   );
--- WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a358e963cb24d01ac66e2bb618b87e4664c08c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8fd25818243661f5fc0d0a0e2fe23552e74ad83")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
