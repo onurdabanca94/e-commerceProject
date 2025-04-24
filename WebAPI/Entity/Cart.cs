8aa45eb1d5a75a1d4dbb790169a894adb14562bf
Onur Dabanca <onurdbnc@gmail.com>
2025-04-24 03:15:27 +0300

    Cart endpoint created.

--- WebAPI/Entity/Cart.cs
+++ WebAPI/Entity/Cart.cs
@@ -5,6 +5,34 @@ public class Cart
     public int CartId { get; set; }
     public string CustomerId { get; set; } = null!;
     public List<CartItem> CartItems { get; set; } = new();
+
+    public void AddItem(Product product, int quantity)
+    {
+        var item = CartItems.Where(ci => ci.ProductId == product.Id).FirstOrDefault();
+
+        if(item == null)
+        {
+            CartItems.Add(new CartItem{ Product = product, Quantity = quantity});
+        }
+        else
+        {
+            item.Quantity += quantity;
+        }
+    }
+
+    public void DeleteItem(int productId, int quantity)
+    {
+        var item = CartItems.Where(ci => ci.ProductId == productId).FirstOrDefault();
+
+        if(item == null) return;
+        item.Quantity -= quantity;
+
+        if (item.Quantity == 0)
+        {
+            CartItems.Remove(item);
+        }
+
+    }
 }
 
 public class CartItem
